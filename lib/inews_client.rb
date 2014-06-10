e23804d321fb5879eb1d3daba2150088c4bdf8b5
Gilad Barzilay <g.barzilay@applicaster.com>
2014-06-10 17:52:43 +0300

    change watch method

--- lib/inews_client.rb
+++ lib/inews_client.rb
@@ -4,7 +4,7 @@ require 'inews_client/system'
 require 'inews_client/story_entry'
 
 module InewsClient
-  def watch_queue(&block)
+  def self.watch_queue(&block)
     fired_stories = []
     InewsClient::System.session do |inews|
       inews.queue.with_queue(ENV['current_queue']) do |queue|
